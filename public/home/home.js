315cfc7fcbf8bf40c71394245038c69576183cb8
Priyanka <priyankashah3107@gmail.com>
2024-07-28 01:59:17 +0530

    feat: homeimg

---
+++ public/home/home.js
@@ -0,0 +1,24 @@
+const homeimg = [
+  {
+    id: 1,
+    img: "./home/h1.png"
+  },
+  {
+    id: 2,
+    img: "./home/h2.png"
+  },
+  {
+    id: 3,
+    img: "./home/h3.png"
+  },
+  {
+    id: 4,
+    img: "./home/h4.png"
+  },
+  {
+    id: 5,
+    img: "./home/h5.png"
+  },
+]
+
+export default homeimg;
