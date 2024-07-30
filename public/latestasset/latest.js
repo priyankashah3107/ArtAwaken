fc958f4c6d98fdf7616c39d7647d6df52cdd91b6
Priyanka <priyankashah3107@gmail.com>
2024-07-30 16:07:10 +0530

    feat: set cardAssest

---
+++ public/latestasset/latest.js
@@ -0,0 +1,113 @@
+import ButtonTwo from "../../src/projectcomponet/Button"
+const latestwork = [
+  { 
+    id: 1,
+    img: "/latestasset/shiv.png",
+    price: "₹ 20k",
+    tit: "Shiv Shakti",
+    name: "universe",
+    category: "Oil",
+    buy: <ButtonTwo value={"Buy Now"} />
+  },
+  { 
+    id: 2,
+    img: "/latestasset/daisy.png",
+    price: "₹ 20k",
+    tit: "White Daisy",
+    name: "Shiv",
+    category: "Watercolor",
+    buy: <ButtonTwo value={"Buy Now"} />
+  },
+  { 
+    id: 3,
+    img: "/latestasset/gilhari.png",
+    price: "₹ 20k",
+    tit: "Tired Squirrel",
+    name: "Shiva",
+    category: "Photograph",
+    buy: <ButtonTwo value={"Buy Now"} />
+  },
+  { 
+    id: 4,
+    img: "/latestasset/lotus.png",
+    price: "₹ 20k",
+    tit: "Lotus",
+    name: "universe",
+    category: "Pastels",
+    buy: <ButtonTwo value={"Buy Now"} />
+  },
+  { 
+    id: 5,
+    img: "/latestasset/lovers.png",
+    price: "₹ 20k",
+    tit: "Lovers",
+    name: "Unknown",
+    category: "Scluptures",
+    buy: <ButtonTwo value={"Buy Now"} />
+  },
+  { 
+    id: 6,
+    img: "/latestasset/model.png",
+    price: "₹ 20k",
+    tit: "Birthday Dress",
+    name: "Shiva",
+    category: "Fashion Design",
+    buy: <ButtonTwo value={"Buy Now"} />
+  },
+  { 
+    id: 7,
+    img: "/latestasset/girl.png",
+    price: "₹ 20k",
+    tit: "Women",
+    name: "shiva",
+    category: "Digital Art",
+    buy: <ButtonTwo value={"Buy Now"} />
+  },
+  { 
+    id: 8,
+    img: "/latestasset/eye.png",
+    price: "₹ 20k",
+    tit: "An Eye",
+    name: "Max",
+    category: "Digital Art",
+    buy: <ButtonTwo value={"Buy Now"} />
+  },
+  { 
+    id: 9,
+    img: "/latestasset/pri.png",
+    price: "₹ 20k",
+    tit: "Female",
+    name: "shiva",
+    category: "Portraits",
+    buy: <ButtonTwo value={"Buy Now"} />
+  },
+  { 
+    id: 10,
+    img: "/latestasset/dancer.png",
+    price: "₹ 20k",
+    tit: "Dance",
+    name: "Shiv",
+    category: "Pastels",
+    buy: <ButtonTwo value={"Buy Now"} />
+  },
+  { 
+    id: 11,
+    img: "/latestasset/shiv.png",
+    price: "₹ 20k",
+    tit: "Forest Lake LandScape  ",
+    name: "Priyanka",
+    category: "Oil",
+    buy: <ButtonTwo value={"Buy Now"} />
+  },
+  { 
+    id: 12,
+    img: "/latestasset/flower.png",
+    price: "₹ 20k",
+    tit: "Nature",
+    name: "Universe",
+    category: "Photograph",
+    buy: <ButtonTwo value={"Buy Now"} />
+  }
+]
+
+export default latestwork;
