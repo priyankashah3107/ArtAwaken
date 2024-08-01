15a55564a1b957f431bbcf6e4b91bfdf7778723a
Priyanka <priyankashah3107@gmail.com>
2024-08-02 01:51:23 +0530

    little changes

--- src/app/pages/scluptures/layout.tsx
+++ src/app/pages/scluptures/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter, Sansita_Swashed } from "next/font/google";
-import "./globals.css";
+import "../../globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 const sansita_swashed = Sansita_Swashed({subsets:["latin"], weight: "300",})
