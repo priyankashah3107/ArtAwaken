92bb80095ea63d6ac4bc9f325e84e40c27a45464
Priyanka <priyankashah3107@gmail.com>
2024-08-12 02:16:47 +0530

    fix

--- src/projectcomponet/Home.tsx
+++ src/projectcomponet/Home.tsx
@@ -54,10 +54,10 @@ const Home = () => {
             {/* <ButtonTwo value={"Explore Now"} /> */}
             <button className='btnMain  md:mt-6 md:rounded-xl md:w-[200px] md:h-[50px] md:text-xl'>Explore Now</button>
           </div>
-          <div className='hidden lg:block'>
-              <HomeCarousel />
-              </div> 
         </div>
+        <div className='hidden lg:block'>
+              <HomeCarousel />
+          </div> 
       </div>
     </>
   )
