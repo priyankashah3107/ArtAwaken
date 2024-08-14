f6decc68c06403a362bbf4b1fe41a4aeab61cd36
Priyanka <shahpriyanka9971@gmail.com>
2024-08-14 09:07:50 -0700

    getDataToke + Me

---
+++ src/app/api/users/me/route.ts
@@ -0,0 +1,29 @@
+import {connectToMongoDb} from "@/dbConfig/connectToDb"
+import { getDataFromToken } from "@/helpers/getDataFromToken"
+import User from "@/models/userModels"
+import {NextRequest, NextResponse} from 'next/server'
+
+connectToMongoDb()
+
+export async function POST(request: NextRequest) {
+    try {
+        const userId = await getDataFromToken(request)
+        if (!userId) {
+            return NextResponse.json({ message: "Invalid Token" }, { status: 401 });
+        }
+        const user = await User.findOne({_id: userId}).select("-password")
+       
+        if(!user) {
+            return NextResponse.json("User not exists")
+        }
+
+       return NextResponse.json({
+        message: "User found",
+        data: user
+       })
+ 
+    } catch (error: any) {
+        console.error("Error in me Route", error.message);
+        return NextResponse.json({error: error.message}, {status: 500})
+    }
+}
---
+++ src/helpers/getDataFromToken.ts
@@ -0,0 +1,16 @@
+import { NextRequest } from "next/server";
+import jwt from 'jsonwebtoken'
+
+export const getDataFromToken = async (request: NextRequest) => {
+    try {
+        const token = request.cookies.get("token")?.value || "";
+        if (!token) {
+            throw new Error("Token not found");
+        }
+        const decodedToken: any = jwt.verify(token, process.env.TOKEN_SECRET!)
+        return decodedToken.id;  
+    } catch (error: any) {
+        console.error("Token verification failed:", error.message);
+        throw new Error(error)
+    }
+}
